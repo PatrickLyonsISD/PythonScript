e9b82fef8d72b7a21aa1d388352e3e0410cd0572
Patrick Lyons <43409140+PatrickLyonsISD@users.noreply.github.com>
2024-04-21 10:42:11 +0100

    adding comments

--- pi_stats.py
+++ pi_stats.py
@@ -1,3 +1,19 @@
+"""
+Pi System Stats Collection Script
+
+This script runs on a Raspberry Pi and interfaces with Firebase to track and store the Pi device's system statistics.
+It is designed to periodically gather system performance metrics such as CPU usage, memory utilization, disk usage,
+and a timestamp indicating the last update. These metrics are then sent to Firebase, ensuring that device performance
+can be monitored remotely in real-time.
+
+Key Functionalities:
+1. System Statistics Gathering: Utilizes the 'psutil' library to access system performance data, capturing 
+   current CPU percentage, memory usage, disk usage, and the exact time of the data snapshot.
+2. Firebase Integration: Initializes a connection to Firebase using the Firebase Admin SDK, where it updates the 
+   'pi_stats' node with the latest system statistics.
+3. Scheduled Updates: Designed to run as a recurring task, the script collects and updates the performance data at 
+   regular intervals (as determined by the deployment setup), ensuring ongoing monitoring.
+"""
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
